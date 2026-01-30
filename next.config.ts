bbc0cfc8884fe238145f7da109a86b9a5aa90aa1
Hiro Aozora <hiroaozora94@gmail.com>
2026-01-31 02:21:02 +0700

    masang pwa

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // @ts-ignore
+  turbopack: {},
   images: {
     remotePatterns: [
       {
@@ -27,4 +29,11 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+export default withPWA(nextConfig);
