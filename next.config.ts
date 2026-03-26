26ef2a58ab7df99065830902ad60869b1753678e
Hiro Aozora <hiroaozora94@gmail.com>
2026-03-27 00:58:47 +0700

    ganti domain

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "otakudesu.best",
+        hostname: "otakudesu.blog",
       },
       {
         protocol: "https",
